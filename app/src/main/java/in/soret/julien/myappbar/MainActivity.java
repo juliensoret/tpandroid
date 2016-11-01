5ceb0f8979370df5549de20ac23a0afe94e50caa
Julien SORET <julien@soret.in>
2016-11-01 18:10:19 +0100

    Add app bar with example actions

--- app/src/main/java/in/soret/julien/myappbar/MainActivity.java
+++ app/src/main/java/in/soret/julien/myappbar/MainActivity.java
@@ -1,13 +1,46 @@
 package in.soret.julien.myappbar;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { /* AppCompat Activity est important ici. */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Toast.makeText(this, "Option Paramètres selectionnée", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_edit:
+                Toast.makeText(this, "Option Editer selectionnée", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.action_save:
+                Toast.makeText(this, "Option Sauvegarder selectionnée", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
